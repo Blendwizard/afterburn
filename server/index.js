7c87283b7edf222823463ba94e59e6361b430b97
JoeVanCamp <vancampjoe@gmail.com>
2022-05-04 11:20:11 -0700

    Removed redundant app file, fixed server not parsing json payloads

--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Parse json payloads
+app.use(express.json())
+
+// Require router object
+const router = require('./routes');
+
+// app.use(express.static(path.join(__dirname, './public')))
+
+// Use router to direct requests
+app.use('/', router);
 
 const PORT = 3001;
 
